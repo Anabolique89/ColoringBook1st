5a2e4125b082d249d07b95f1eec9f0f023902f38
NinaAndreeaPopovici <ninandreeapopovici@gmail.com>
2020-03-04 21:26:03 +0100

    !st Commit

---
+++ main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", start);
+
+let selectedColor;
+
+async function start() {
+  let response = await fetch("pinkLotus.txt.svg");
+  let mySvgData = await response.text();
+  document.querySelector("section").innerHTML = mySvgData;
+  //document.querySelector("h1").classList.add("hidden");
+  startManipulatingTheSvg();
+}
+
+function startManipulatingTheSvg() {
+  //clearColor();
+  document.querySelectorAll(".color").forEach(color => {
+    color.addEventListener("click", e => {
+      selectedColor = e.target.dataset.color;
+      document.querySelector(
+        ".selectedcolorbox"
+      ).style.backgroundColor = selectedColor;
+    });
+  });
+  document.querySelector("button").addEventListener("click", clearColor);
+  document.querySelector("#fills").addEventListener("click", fillColor);
+}
+
+function clearColor() {
+  document
+    .querySelectorAll(".PinkFlower", ".GreenLeaf", "path")
+    .forEach(path => {
+      path.style.fill = "transparent";
+
+      console.log(clearColor);
+    });
+  document.querySelectorAll("#outlines path").forEach(path => {
+    path.style.stroke = "";
+    path.style.fill = "";
+  });
+
+  document.querySelector("body").style.backgroundColor = "";
+  document.body.style.backgroundImage = "url('')";
+  document.querySelector(".selectedcolorbox").style.backgroundColor =
+    "transparent";
+}
+
+function fillColor(e) {
+  e.target.style.fill = selectedColor;
+}
+/*
+function rainbow() {
+  console.log(rainbow);
+  document.querySelectorAll("#fills path, polygon, polyline").forEach(path => {
+    path.style.fill = "black";
+  });
+  document.querySelectorAll("#outlines").forEach(path => {
+    path.style.stroke = "oldlace";
+    path.style.fill = "red";
+  });
+
+  document.body.style.backgroundImage = "url('darkforest.jpg')";
+  document.querySelector("h1").style.color = "ghostwhite";
+}
+*/
